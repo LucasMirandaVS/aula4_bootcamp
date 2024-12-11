9a449560e901626dafa693d89edd527ebbcf78c7
Lucas <lucas-mvs@hotmail.com>
2024-12-11 18:45:46 -0300

    subindo o primeiro exercicio

---
+++ exercicio.py
@@ -0,0 +1,20 @@
+# Dicionário que armazena as informações de um livro:
+livro1: dict = {
+    "Título": "Deserto dos Tartaros",
+    "Autor": "Dino Buzatti",
+    "Ano": 1955
+}
+
+livro2: dict = {
+    "Título": "Dracula",
+    "Autor": "Bram Stoker",
+    "Ano": 1899
+}
+
+lista_de_livros: list = []
+
+lista_de_livros.append(livro1)
+lista_de_livros.append(livro2)
+
+print(lista_de_livros)
+
